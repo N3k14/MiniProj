defc31216e957765283130f16c89c3a70e09ce6c
N3k14 <nikita.belik.2020@mail.ru>
2025-06-25 18:03:15 +0300

    add migrations

---
+++ Backend/Infrastructure/Migrations/20250625131759_Init_SaleContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init_SaleContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "sales");
+
+            migrationBuilder.CreateTable(
+                name: "order",
+                schema: "sales",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    number = table.Column<string>(type: "text", nullable: false),
+                    cargo_weight_in_kg = table.Column<decimal>(type: "numeric", nullable: false),
+                    cargo_pickup_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    receiver_address = table.Column<string>(type: "text", nullable: false),
+                    receiver_city = table.Column<string>(type: "text", nullable: false),
+                    sender_address = table.Column<string>(type: "text", nullable: false),
+                    sender_city = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_order", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order",
+                schema: "sales");
+        }
+    }
+}
