d30e7cf907bbe01dac6f2fe85e890d739e74a186
N3k14 <nikita.belik.2020@mail.ru>
2025-06-25 17:55:51 +0300

    add create method and constructor

--- Backend/Domain/ValueObjects/Location.cs
+++ Backend/Domain/ValueObjects/Location.cs
@@ -4,6 +4,19 @@ public class Location
 {
     public string City { get; private set; }
     public string Address { get; private set; }
+
+    private Location(string city, string address)
+    {
+        City = city;
+        Address = address;
+    }
+
+    public static Location Create(string city, string address)
+    {
+        ArgumentNullException.ThrowIfNull(city);
+        ArgumentNullException.ThrowIfNull(address);
+        
+        return new Location(city, address);
+    }
     
-    // todo сделать проверки для входных данных
 }
